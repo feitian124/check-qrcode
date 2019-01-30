c4181325f8e4c366993cdf4320b2fb12d3f41e6b
zhou.peng <zhou.peng>
2019-01-30 11:40:43 +0800

    show images in console

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import _ from 'lodash';
+import jsQR from "jsqr";
+import _ from 'lodash'
 
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    element.innerHTML = _.join(['check', 'qrcode'], ' ');
-  
-    return element;
+  var element = document.createElement('div');
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  element.innerHTML = _.join(['check', 'qrcode'], ' ');
+
+  var imgs = document.getElementsByTagName("img");
+  for (var i=0, len=imgs.length; i<len; i++) {
+    console.log(imgs[i]);
   }
+
+  return element;
+}
   
-  document.body.appendChild(component());
+document.body.appendChild(component());
