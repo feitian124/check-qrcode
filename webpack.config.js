2a25a1e72b184f621a17b670dbb86b3cce676617
zhou.peng <zhou.peng>
2019-01-30 16:45:48 +0800

    put static in dist

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
-      contentBase: './dist'
+      contentBase: './dist',
+      publicPath: '/static/'
   },
   output: {
     filename: 'bundle.js',
