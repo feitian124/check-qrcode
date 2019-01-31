6280b79b263daf9c3ccd73d180d83b223d138d0e
zhou.peng <zhou.peng>
2019-01-31 19:48:09 +0800

    异常处理及使用禁止二维码

--- src/index.js
+++ src/index.js
@@ -5,18 +5,17 @@ async function check() {
 
   var imgs = document.getElementsByTagName("img");
   for (var i=0, len=imgs.length; i<len; i++) {
-
-    var {data, width, height} = await pixels(imgs[i].src);
-
-    const code = jsQR(data, width, height);
-
-    if (code) {
-      console.log(imgs[i], " Found QR code, value is: ", code.data);
-      imgs[i].src = '/static/forbidden.jpg';
+    try {
+      var {data, width, height} = await pixels(imgs[i].src);
+      const code = jsQR(data, width, height);
+      if (code) {
+        console.log(imgs[i], " Found QR code, value is: ", code.data);
+        imgs[i].src = 'http://iph.href.lu/' + width + 'x'+ height +'?text=禁';
+      }
+    } catch (error) {
+      console.error('get image data error:', imgs[i].src)
     }
-
   }
-
 }
 
 check();
