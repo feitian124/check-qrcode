7ad379fd27f74403febfecfbc30c9b0d0912a282
peng.yun <feitian124@qq.com>
2019-04-29 18:09:50 +0800

    可以动态加载图片

--- src/index.js
+++ src/index.js
@@ -18,4 +18,43 @@ async function check() {
   }
 }
 
+/**
+ * 全局 ajax 事件监听器, 用于检测动态加载的图片
+ */
+function addAjaxListener() {
+  var req = new Object();
+  req.tempOpen = XMLHttpRequest.prototype.open;
+  req.tempSend = XMLHttpRequest.prototype.send;
+  req.callback = function () {
+    // this.method :the ajax method used
+    // this.url    :the url of the requested script (including query string, if any) (urlencoded) 
+    // this.data   :the data sent, if any ex: foo=bar&a=b (urlencoded)
+    console.log('ajax method:' + req.method);
+    console.log('ajax url:' + req.url);
+    console.log('ajax data:' + req.data);
+    check();
+  }
+  
+  XMLHttpRequest.prototype.open = function(a,b) {
+    if (!a) var a='';
+    if (!b) var b='';
+    req.tempOpen.apply(this, arguments);
+    req.method = a;  
+    req.url = b;
+    if (a.toLowerCase() == 'get') {
+      req.data = b.split('?');
+      req.data = req.data[1];
+    }
+  }
+  
+  XMLHttpRequest.prototype.send = function(a,b) {
+    if (!a) var a='';
+    if (!b) var b='';
+    req.tempSend.apply(this, arguments);
+    if(req.method.toLowerCase() == 'post')req.data = a;
+    req.callback();
+  }
+}
+
+addAjaxListener();
 check();
